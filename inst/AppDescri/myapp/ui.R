0edade8a9eb4782dcf04fb6f11dcaa86bf1b3d0b
Cécile Rodrigues <cecile.rodrigues@openmailbox.org>
2017-10-11 15:24:54 +0200

    Add files via upload

--- inst/AppDescri/myapp/ui.R
+++ inst/AppDescri/myapp/ui.R
@@ -5,12 +5,12 @@ library(ggplot2)
 
 # --------------------------- TITRE -------------------------------#
 
-shinyUI(  
+shinyUI(
   navbarPage("Descri",
-             
-             
+
+
              #--------------------------- ONGLET importation et filtres -------------------------#
-             
+
              tabPanel("1. Importation et sélection des individus",
                       fluidPage(
                         tags$head(tags$style(HTML(
@@ -21,37 +21,37 @@ shinyUI(
                           #           ".shiny-output-error:before { visibility: hidden; }"
                         )
                         )
-                        
+
                         ,theme=shinytheme("united"),
-                        (column(12,h6('ATTENTION : Avant toute action, 
+                        (column(12,h6('ATTENTION : Avant toute action,
                                     cliquer sur "Open in browser", sans quoi les
                                     boutons et téléchargements ne fonctionneront pas.'))),
-                        
+
                         column(5,h4("1. Fichier à charger (.txt ou .csv)"),
                                h6("La table brute est une base de données dont les lignes correspondent aux individus statistiques.
-                                  Elle doit être en format texte (.txt ou .csv) ; Le délimitateur, 
+                                  Elle doit être en format texte (.txt ou .csv) ; Le délimitateur,
                                   l'extension du fichier et l'encodage des caractères sont précisés.
                                   Elle est importée en passant par le bouton \"browse\"."),
                                wellPanel(
                                  uiOutput("donnees.fichier.ui")),
                                h4("2. Détail des variables importées"),
-                               h6("Vérifier que le tableau a correctement été 
+                               h6("Vérifier que le tableau a correctement été
                                   importé à l'aide du résumé suivant :"),
                                textOutput("Dimensions"),
                                tableOutput("Resume")),
                         column(7,
-                               fluidRow(column(6, 
+                               fluidRow(column(6,
                                                h4("3. Choix de l'identifiant (Obligatoire)"),
                                                uiOutput("SelectID")),
                                         column(6,
                                                h6("Si la table ne contient pas d'identifiant
-                                                  unique, le bouton ci-dessous permet de 
+                                                  unique, le bouton ci-dessous permet de
                                                   télécharger la table à laquelle sera ajoutée
                                                   une variable \"ID\""),
                                                downloadButton("PasDID",'Télécharger avec ajout d\'un identifiant ("ID")'))
                                ),
-                               
-                               
+
+
                                h4("4. Sélection des individus / Filtres (Optionnel)"),
                                h6("Ces filtres peuvent réduire et préciser la population étudiée. Ce sont des filtres logiques.
                                   Si on ne souhaite aucune sélection, penser à sélectionner un choix vide sur chaque champ."),
@@ -66,10 +66,10 @@ shinyUI(
                                                   selectInput("Operateur1", "Opérateur :",
                                                               choices=as.list(c(" ","=","diff. de",">",">=","<","<=")))),
                                            column(5,
-                                                  
+
                                                   uiOutput("Select")))
-                                 
-                                 
+
+
                                ),
                                selectInput("OperateurMid","",
                                            choices=as.list(c(" ","OU","ET")), selected = NULL),
@@ -81,9 +81,9 @@ shinyUI(
                                                   selectInput("Operateur2", "Opérateur :",
                                                               choices=as.list(c(" ","=","diff. de",">",">=","<","<=")))),
                                            column(5,
-                                                  
+
                                                   uiOutput("Select2")))),
-                               
+
                                selectInput("OperateurMid2","",
                                            choices=as.list(c(" ","OU","ET")), selected = NULL),
                                wellPanel(
@@ -94,10 +94,10 @@ shinyUI(
                                                   selectInput("Operateur3", "Opérateur :",
                                                               choices=as.list(c(" ","=","diff. de",">",">=","<","<=")))),
                                            column(5,
-                                                  
+
                                                   uiOutput("Select3")))
                                ),
-                               
+
                                selectInput("OperateurMid3","",
                                            choices=as.list(c(" ","OU","ET")), selected = NULL),
                                wellPanel(
@@ -108,23 +108,23 @@ shinyUI(
                                                   selectInput("Operateur4", "Opérateur :",
                                                               choices=as.list(c(" ","=","diff. de",">",">=","<","<=")))),
                                            column(5,
-                                                  
+
                                                   uiOutput("Select4")))))
                         )),
-             
-             
+
+
              #--------------------------- ONGLET tris à plat / croisés -------------------------#
-             
+
              tabPanel("2. Tris à plat et tris croisés",
                       fluidPage(
-                             
+
                              h4("1. Choix des variables"),
                              h6("- Si les listes de variables ne s'affichent pas, vérifier que
                                 l'importation a correctement fonctionné et qu'un identifiant a été
                                 sélectionné (onglet 1. Importation et sélection)."),
                               h6  ("- Pour un tri à plat, sélectionner uniquement la 1ère variable et
                                  laisser la 2ème vide (première option)."),
-                             h6  ("  - Pour un tri croisé, sélectionner la 1ère variable (représentée en 
+                             h6  ("  - Pour un tri croisé, sélectionner la 1ère variable (représentée en
                                 lignes) et la 2ème (en colonnes)."),
                                h6  (" - Les types (qualitatif ou quantitatif) des variables sont modifiables."),
                                   h6  ("  - Si une variable est qualitative et l'autre quantitative,
@@ -141,21 +141,21 @@ shinyUI(
                                                            "Logique")), selected = "Pas de modification"))),
                       fluidRow( column(6,
                               uiOutput("SelectVarCrois2")),
-                       column(6, 
-                              textOutput("TypeVar2"),     
+                       column(6,
+                              textOutput("TypeVar2"),
                              selectInput("TypeVarCrois2","",
                                          choices=as.list(c("Pas de modification","Qualitative",
                                                            "Quantitative - entier",
                                                           # "Quantitative - réel",
                                                            "Logique")), selected = "Pas de modification"))),
-                        fluidRow   (column(12,     
+                        fluidRow   (column(12,
                                            h4("2. Tableau"),
-                      
+
                       uiOutput("ChoixReprez"),
                       uiOutput("h6_Reprez"),
                       uiOutput("ChoixTrier"),
                       uiOutput("ChoixAfficherNA"),
-                      
+
                              tableOutput("Tableau"),
                       textOutput("Population"),
                       hr(),
@@ -165,19 +165,20 @@ shinyUI(
                              h4("Options et choix graphiques"),
                              uiOutput("ChoixGrapheAffichage"),
                              uiOutput("ChoixGrapheOrdonne"),
+                             uiOutput("ChoixGrapheEmpile"),
+                             uiOutput("ChoixAjoutEns"),
+                             uiOutput("ChoixGraphePas"),
                              uiOutput("ChoixGrapheSansNA"),
                              uiOutput("ChoixGrapheSansVide"),
                              uiOutput("ChoixGrapheSansNAVar1"),
                              uiOutput("ChoixGrapheSansNAVar2"),
                              uiOutput("ChoixGrapheSansVideVar1"),
                              uiOutput("ChoixGrapheSansVideVar2"),
-                             uiOutput("ChoixAjoutEns"),
-                             uiOutput("ChoixGraphePas"),
                              textInput("TextTitre","Titre (auto. si vide) :", "")),
-                             
-                             column(9, 
+
+                             column(9,
                                     h4("3. Graphique"),
-                                    
+
                                     plotOutput("Graphique") ,
                                     textOutput("Population2"),
                                     hr(),
@@ -187,14 +188,14 @@ shinyUI(
                                     column(3, hr(),downloadButton("DlGraphe","Télécharger le graphe"))
                              ))
                       )),
-             
+
              #--------------------------- SUPPRIMER messages d'erreur -------------------------#
-             
+
              tags$style(type="text/css",
                         ".shiny-output-error { visibility: hidden; }",
                         ".shiny-output-error:before { visibility: hidden; }")
-             
-             
-           
+
+
+
   )
 )
